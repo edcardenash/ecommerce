eae6ad1695797109d72f37e2199824ef8772df30
Edgar Cardenas <edcardenashorta@gmail.com>
2023-05-12 17:05:22 -0400

    admin sessions controller and route setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   root to: "pages#home"
+  namespace :api do
+    namespace :v1 do
+      devise_for :admins, controllers: { sessions: 'api/v1/admins/sessions' }, defaults: { format: :json }
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
