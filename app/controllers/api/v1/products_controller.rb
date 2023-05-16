115a21ecd085a3a444395322ec5a15ffeb0b4415
Edgar Cardenas <edcardenashorta@gmail.com>
2023-05-16 16:00:23 -0400

    logic to save api cache

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,22 +4,26 @@ module Api
       before_action :set_product, only: [:show, :update, :destroy]
 
       def top_products
-        top_products_per_category = Category.includes(:top_products).map do |category|
-          {
-            category_name: category.name,
-            top_product: category.top_products.first
-          }
+        top_products_per_category = Rails.cache.fetch("top_products_per_category", expires_in: 1.hour) do
+          Category.includes(:top_products).map do |category|
+            {
+              category_name: category.name,
+              top_product: category.top_products.first
+            }
+          end
         end
 
         render json: top_products_per_category
       end
 
       def top_revenue_products
-        top_revenue_products_per_category = Category.includes(:top_revenue_products).map do |category|
-          {
-            category_name: category.name,
-            top_revenue_products: category.top_revenue_products(3)
-          }
+        top_revenue_products_per_category = Rails.cache.fetch("top_revenue_products_per_category", expires_in: 1.hour) do
+          Category.includes(:top_revenue_products).map do |category|
+            {
+              category_name: category.name,
+              top_revenue_products: category.top_revenue_products(3)
+            }
+          end
         end
 
         render json: top_revenue_products_per_category
